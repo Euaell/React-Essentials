944a8d3c7f50693feb3bf4cb41252d8e77b38030
Euael Eshete <euaelesh@outlook.com>
2022-05-09 08:46:01 +0300

    some clean up

--- src/index.js
+++ src/index.js
@@ -2,16 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+root.render(
+	
+	<App />
+	
+	// <div>
+	// 	<h1 style={{ color: 'blue'}}> Hello, World! </h1>
+	// 	<ul>
+	// 		<li>Monday</li>
+	// 		<li>Tuesday</li>
+	// 		<li>Wednesday</li>
+	// 		<li>Thursday</li>
+	// 	</ul>
+	// </div>
+
+
+	// React.createElement(
+	// 	"div",
+	// 	null, 
+	// 	React.createElement("h1", {style: {color: "blue"}}, "Hello, World!"),
+	// 	React.createElement(
+	// 		"ul",
+	// 		null,
+	// 		React.createElement("li", null, "monday"),
+	// 		React.createElement("li", null, "Tuesday"),
+	// 		React.createElement("li", null, "Wednesday"),
+	// 	))
+	
+);
