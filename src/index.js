25ce9549e90981729722121e0a623edcd696ae28
Euael Eshete <euaelesh@outlook.com>
2022-05-09 08:49:21 +0300

    done

--- src/index.js
+++ src/index.js
@@ -10,27 +10,4 @@ root.render(
 	
 	<App />
 	
-	// <div>
-	// 	<h1 style={{ color: 'blue'}}> Hello, World! </h1>
-	// 	<ul>
-	// 		<li>Monday</li>
-	// 		<li>Tuesday</li>
-	// 		<li>Wednesday</li>
-	// 		<li>Thursday</li>
-	// 	</ul>
-	// </div>
-
-
-	// React.createElement(
-	// 	"div",
-	// 	null, 
-	// 	React.createElement("h1", {style: {color: "blue"}}, "Hello, World!"),
-	// 	React.createElement(
-	// 		"ul",
-	// 		null,
-	// 		React.createElement("li", null, "monday"),
-	// 		React.createElement("li", null, "Tuesday"),
-	// 		React.createElement("li", null, "Wednesday"),
-	// 	))
-	
 );
