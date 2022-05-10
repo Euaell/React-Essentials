76f71fa09a4a25f55863b3acf6907c1b780454c1
Euael Eshete <euaelesh@outlook.com>
2022-05-10 22:09:42 +0300

    logo

--- src/header.js
+++ src/header.js
@@ -1,8 +1,12 @@
+import lion from "./lion.png";
 function Header() {
     return ( 
         <nav>
             <div className="nav-wrapper inconsolata teal lighten-1">
-                <a href="/" className="brand-logo center font-900">Github Users</a>
+                <a href="/" className="brand-logo center font-900">
+                    <img height={40} alt="lion" src={lion} />
+                    <span style={{marginTop: 0}}>Github Users</span>
+                </a>
                 <ul id="nav-mobile" className="left hide-on-med-and-down">
                     <li className="active"><a href="/">Home</a></li>
                     <li><a href="/">Components</a></li>
