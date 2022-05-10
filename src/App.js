7f54311229737db31dacaab58de9b2c1320c442b
Euaell <euaelmeko@gmail.com>
2022-05-10 15:53:16 +0300

    comp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import React, {useState, useEffect} from "react";
+import React, {useReducer} from "react";
 import './App.css';
 
 function App(props) {
-	const [emotion, setEmotion] = useState("Happy");
-	const [secondary, setSecondary] = useState("Tired");
-
-	useEffect(() => {
-		console.log(`It's ${emotion} in here.`);
-	}, [emotion])
-
-	useEffect(() => {
-		console.log(`It's ${secondary} in here.`);
-	}, [secondary])
+	const [checked, toggle] = useReducer(
+		(checked) => !checked,
+		false
+	);
 
 	return (
 		<>
-			<h1>Feeling {emotion} and {secondary}</h1>
-			<button onClick={() => {setEmotion("Happy"); setSecondary("Tired")}}>Make Happy</button>
-			<button onClick={() => setEmotion("Annoyed")}>Annoy</button>
-			<button onClick={() => setEmotion("Mad")}>Make angry</button><br />
-			<button onClick={() => setSecondary("Cranky")}>Crank</button>
+			<input type="checkbox" value={checked} onChange={toggle}/>
+			<p>{checked ? "Checked" : "Unchecked"}</p>
 		</>
 	)
 }
